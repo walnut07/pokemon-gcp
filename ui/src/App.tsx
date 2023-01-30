c0e73957ea9101646589847de6e08e4a4da2af24
walnut07 <p20080mk@gm.tsuda.ac.jp>
2023-01-30 19:20:43 +0900

    get doubleDamageTo on submitting form

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
 
 import Input from './components/Input';
+import { useState } from 'react';
 
 function App() {
+  const [doubleDamageTo, setDoubleDamageTo] = useState<Array<String>>([]);
+
   return (
     <div className="App">
-      <Input />
+      <Input setDoubleDamageTo={setDoubleDamageTo}/>
     </div>
   );
 }
