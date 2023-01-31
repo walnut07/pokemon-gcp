67b8c8baaf84f81b7a5134fecffa335eb1340bc4
walnut07 <p20080mk@gm.tsuda.ac.jp>
2023-01-31 18:16:41 +0900

    get response doubleDamageTo on submitting a pokemon name

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
 
+import DoubleDamageTo from './components/DoubleDamageTo';
 import Input from './components/Input';
+import { useState } from 'react';
 
 function App() {
+  const [doubleDamageTo, setDoubleDamageTo] = useState<Array<Object>>([]);
+
   return (
     <div className="App">
-      <Input />
+      <Input setDoubleDamageTo={setDoubleDamageTo}/>
+      <DoubleDamageTo doubleDamageTo={doubleDamageTo}></DoubleDamageTo>
     </div>
   );
 }
--- ui/src/components/Input.tsx
+++ ui/src/components/Input.tsx
@@ -1,12 +1,26 @@
-function Input() {
+import { Dispatch, SetStateAction } from "react";
+
+import {PokeTypeApi} from "../API/api";
+
+function Input({ setDoubleDamageTo } : {setDoubleDamageTo: Dispatch<SetStateAction<Object[]>>}) {
+    const displayDoubleDamageTo = async (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        const input = document.getElementById('poke-name') as HTMLInputElement;
+        const doubleDamageTo = await new PokeTypeApi(input.value).getDoubleDamageTo();
+        setDoubleDamageTo(doubleDamageTo);
+    }
 
     return (
-        <div>
-            <label htmlFor="small-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pokemon Name</label>
-            <input type="text" id="small-input" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 
+        <form>
+            <label htmlFor="poke-name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pokemon Name</label>
+            <input type="text" id="poke-name" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 
                 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
-                dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
-        </div>
+                dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+                type="submit" onClick={displayDoubleDamageTo}>
+                Search
+            </button>
+        </form>
     )
 }
 
